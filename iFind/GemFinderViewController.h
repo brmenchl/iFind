906de64dc6001368c466398818a265d52e276f62
Andrew <andrew.milenius@gmail.com>
2014-02-25 18:31:43 -0500

    added GemFinder view

---
+++ iFind/GemFinderViewController.h
@@ -0,0 +1,14 @@
+//
+//  GemFinderViewController.h
+//  iFind
+//
+//  Created by Andrew Milenius on 2/25/14.
+//  Copyright (c) 2014 FuarkNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface GemFinderViewController : UIViewController
+
+@end
