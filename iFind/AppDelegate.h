8a2820a458ef333972b2eb0039a6da27addd9528
Andrew <andrew.milenius@gmail.com>
2014-02-25 17:18:18 -0500

    Initial Commit

---
+++ iFind/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iFind
+//
+//  Created by Andrew Milenius on 2/25/14.
+//  Copyright (c) 2014 FuarkNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
